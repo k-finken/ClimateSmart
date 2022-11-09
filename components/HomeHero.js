9caf7a1466796f9ed20459a29dae0f20ea7ba7c7
k-finken <91611170+k-finken@users.noreply.github.com>
2022-11-08 21:29:22 -0500

    fix
    fix

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import browser from '../images/browser.svg'
+import browser from '../public/browser.svg'
 
 export default function HomeHero() {
     return (
