0c043829ecfb4a375da4381a8fbd627da53eb208
k-finken <91611170+k-finken@users.noreply.github.com>
2022-11-08 21:27:36 -0500

    update index and fix npm
    Fixed npm package file and updated index page via homehero component

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
+import browser from '../images/browser.svg'
 
 export default function HomeHero() {
     return (
         <div className="w-full h-700px">
-            <h1 className="absolute w-1/6 h-132px text-6xl left-1/10 top-1/8">What is ClimateSmart</h1>
+            <div className="float-right mt-16 mr-44">
+                <Image src={browser} alt="Picture of the author" width="670px" height="480px"/>
+            </div>
+            <h1 className="absolute w-1/5 h-132px text-6xl left-44 top-44">What is ClimateSmart?</h1>
+            <p className="absolute w-1/5 h-132px text-1xl left-44 top-80">ClimateSmart description</p>
+            <button className="absolute w-1/5 h-14 text-2xl left-44 top-1/2 border rounded-xl hover:text-themeDark bg-themeLightest hover:scale-110 transition">Call to action</button>
         </div>
     )
 }
