cf96c4d521fb05278944ad32a359892981327238
NP <nikhil.pandya@ufl.edu>
2022-11-08 20:54:18 -0500

    added video page

---
+++ pages/videos.js
@@ -0,0 +1,13 @@
+import HomeHero from '../components/HomeHero'
+import NavBar from '../components/NavBar'
+//import Flowplayer from '../node_modules/@flowplayer/react-flowplayer/lib'
+
+
+export default function Videos() {
+  return (
+    <>
+      <NavBar/>
+      {/* <Flowplayer token="<my-token>" src="//edge.flowplayer.org/bauhaus.mp4" /> */}
+    </>
+  )
+}
