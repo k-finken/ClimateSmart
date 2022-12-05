52333f591ed4553c8ccf105f7c103179f3b5a591
tj-schultz <t.schultz@ufl.edu>
2022-12-05 18:57:26 -0500

    Update ToolsCatalog.js

--- components/ToolsCatalog.js
+++ components/ToolsCatalog.js
@@ -4,12 +4,38 @@ import ReactDOM from "react-dom/client";
 
 export default function ToolsCatalog() {
     
-    const [value, setValue] = useState("");
+    var currentDateObj = new Date();
     
-    let plastic = "\u{1F9F4}"
+    const [value, setValue] = useState("______");
+    const [decompDate, setDecompDate] = useState("");
+    
+    const waste = {
+        plastic: 365000,
+        paper: 42,
+        metal: 36500,
+        organic: 32
+    }
     
     const handleItemClick = value => {
+        var numberOfMlSeconds = currentDateObj.getTime();
+        let de = 0;
+        if (value == "plastic") {
+            de = waste.plastic;
+        }
+        else if (value == "paper"){
+            de = waste.paper;
+        }
+        else if (value == "metal"){
+            de = waste.metal;
+        }
+        else {
+            de = waste.organic;
+        }
+        var addMlSeconds = de * 24 * 60 * 60 * 1000;
+        var newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
+        var decompDate = newDateObj.toDateString();
         setValue(value);
+        setDecompDate(decompDate);
         console.log(`Waste item selected:`, value);
     };
     
@@ -19,14 +45,15 @@ export default function ToolsCatalog() {
       <p className="text-3xl text-themeDark" style={{ width: 422 }}>Waste Decomposition Catalog</p>
       <div>
     
-        <select>
-          <option value="plastic"></option>
-          <option value="paper"></option>
-            <option value="metal"></option>
-            <option value="organic"></option>
+        <select onChange={handleItemClick}>
+          <option value="plastic">Plastic</option>
+          <option value="paper">Paper</option>
+            <option value="metal">Metal</option>
+            <option value="organic">Organic</option>
         </select>
-
-
+      </div>
+      <div>
+        <p>If you threw away {value} today, it would likely decompose by {decompDate}.</p>
       </div>
       
 </div>
