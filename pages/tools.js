64433effb08a7dcefd05c3e0b43a62373fced585
tj-schultz <t.schultz@ufl.edu>
2022-12-05 21:26:41 -0500

    Update tools.js

--- pages/tools.js
+++ pages/tools.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <>
       <NavBar/>
-      <hr className="border-themeDark"></hr>
-      <div className="flex content-center justify-center">
+      <hr className="border-themeDark content-center"></hr>
+      <div className="flex content-center justify-center items-center space-y-8 space-x-8">
       <ToolsCarbon/>
       <ToolsCatalog/>
       </div>
